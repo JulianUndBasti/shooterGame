e8ab14b3f15c1b408666f13d189ed66a7a3c078a
Sebastian Riesel <68468913+SebastianRiesel@users.noreply.github.com>
2023-07-05 20:32:59 +0200

    Created javafx Maven Project

---
+++ shootergame/src/main/java/de/julian_und_basti/shootergame/SystemInfo.java
@@ -0,0 +1,13 @@
+package de.julian_und_basti.shootergame;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
---
+++ shootergame/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module de.julian_und_basti.shootergame {
+    requires javafx.controls;
+    exports de.julian_und_basti.shootergame;
+}
